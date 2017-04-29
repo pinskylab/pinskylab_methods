6700de0ca70458f124fa4c326aa14015211c380b
The Pinsky Lab <malin.pinsky@gmail.com>
2017-04-29 17:46:04 -0400

    Now handle more transects

--- field/randquads.R
+++ field/randquads.R
@@ -1,7 +1,9 @@
-n = 5
+n = 5 # number of quadrats per transect
+tx = 36 # number of transects
 
-allocs = matrix(NA, nrow=12, ncol=5)
-for(i in 1:12){
+
+allocs = matrix(NA, nrow=tx, ncol=5)
+for(i in 1:tx){
 	locs = sort(floor(runif(n, 0,25)))
 	while(any(diff(locs) == 0)){
 		locs = sort(floor(runif(n, 0,25)))
