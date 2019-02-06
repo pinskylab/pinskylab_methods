ab118f28c5125af11b233a1e4f7ac6d6c9da2576
Michelle Stuart <michelle.stuart@rutgers.edu>
2019-02-06 16:26:06 -0500

    moved files out of procedural notebooks and into appropriate directories

---
+++ genomics/analysis/SEQ21_process_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+nohup ./scripts/P117_process.sh &
+nohup ./scripts/P118_process.sh &
+nohup ./scripts/P119_process.sh &
+nohup ./scripts/P120_process.sh &
+nohup ./scripts/P121_process.sh &
+nohup ./scripts/P122_process.sh &
+nohup ./scripts/P123_process.sh &
+nohup ./scripts/P124_process.sh &
+nohup ./scripts/P125_process.sh &
+nohup ./scripts/P126_process.sh &
+nohup ./scripts/P127_process.sh &
+nohup ./scripts/P128_process.sh &
---
+++ genomics/analysis/process_all.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup ./scripts/P128_process.sh &
